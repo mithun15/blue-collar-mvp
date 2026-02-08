c7d6d81356b8846373dc5dc6df5d9d13efd4ca4f
Mithun Kumar <mithun.kumarr6@gmail.com>
2026-02-08 20:47:56 +0530

    cosmetic change

--- src/app/services/job-posting.service.ts
+++ src/app/services/job-posting.service.ts
@@ -5,14 +5,16 @@ import { map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class JobPostingService {
+  private _baseUrl = 'https://blue-collar-api.onrender.com/';
+  // private _baseUrl = 'http://localhost:3000/';
   private _http = inject(HttpClient);
 
   public createJobPosting(job: JobPostingModel) {
-    return this._http.post('http://localhost:3000/employer/jobposting', job);
+    return this._http.post(`${this._baseUrl}employer/jobposting`, job);
   }
 
   public getAllJobPostings() {
-    return this._http.get('http://localhost:3000/employer/jobposting').pipe(
+    return this._http.get(`${this._baseUrl}employer/jobposting`).pipe(
       map((res: any) => {
         return res.data.map((job: any) => {
           return {
@@ -25,6 +27,6 @@ export class JobPostingService {
   }
 
   public deleteJobPosting(id: string) {
-    return this._http.delete(`http://localhost:3000/employer/jobposting/${id}`);
+    return this._http.delete(`${this._baseUrl}employer/jobposting/${id}`);
   }
 }
